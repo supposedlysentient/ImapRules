f01a69575c8a39b1c1ebb5aa4d84ef29ffd43d62
Freddie Sackur <freddie.git@dustyfox.uk>
2025-02-21 13:49:23 +0000

    wip

--- src/Core/Logging.fs
+++ src/Core/Logging.fs
@@ -35,7 +35,7 @@ let Ansi = {|
     DarkCyan = "\x1b[36m"
     DarkGrey = "\x1b[38;2;169;169;169m"
     White = "\x1b[97m"
-    Reset = "\x1b[39m"
+    Reset = "\x1b[0m"
 |}
 
 type Stream = Config.Stream
